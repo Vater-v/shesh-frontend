62e9de3c43eb4a73d6ef0c8785e7b42fa88088ae
Vater-v <v333iv@gmail.com>
2025-12-16 09:35:48 +0700

    Auto-update: 2025-12-16 09:35:48

--- mobile_app/app/src/main/java/com/hmuriy/shesh/ui/SplashScreen.kt
+++ mobile_app/app/src/main/java/com/hmuriy/shesh/ui/SplashScreen.kt
@@ -2,13 +2,9 @@ package com.hmuriy.shesh.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 
 class SplashScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            // Ваш UI код (Composable функция) или вызов функции SplashScreen()
-        }
     }
 }
