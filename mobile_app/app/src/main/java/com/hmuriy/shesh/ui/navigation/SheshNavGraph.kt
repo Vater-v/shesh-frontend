90d03e0cbea97076f376dde05e86118e43c344fb
Vater-v <v333iv@gmail.com>
2025-12-16 14:15:38 +0700

    Auto-update: 2025-12-16 14:15:38

--- mobile_app/app/src/main/java/com/hmuriy/shesh/ui/navigation/SheshNavGraph.kt
+++ mobile_app/app/src/main/java/com/hmuriy/shesh/ui/navigation/SheshNavGraph.kt
@@ -1,38 +1,72 @@
 package com.hmuriy.shesh.ui.navigation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hmuriy.shesh.ui.WelcomeScreen
-//Здесь живет ваша навигация и реализация переходов
-@Composable
-fun SheshNavGraph() {
-    val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "welcome") {
-        
+// Определение маршрутов (лучше вынести в отдельный файл Screen.kt в будущем)
+sealed class Screen(val route: String) {
+    data object Welcome : Screen("welcome")
+    data object RegisterEmail : Screen("register_email")
+    data object Login : Screen("login")
+}
+
+@Composable
+fun SheshNavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.route
+    ) {
+
         // Экран приветствия
-        composable("welcome") {
+        composable(Screen.Welcome.route) {
             WelcomeScreen(
                 onGoogleSignUpClick = {
-                    // Тут может быть вызов ViewModel или сложная логика
-                    // viewModel.startGoogleAuth() 
+                    // TODO: Реализовать логику Google Auth
                 },
                 onLoginSignUpClick = {
-                    // Простой переход на экран регистрации по Email
-                    navController.navigate("register_email")
+                    navController.navigate(Screen.RegisterEmail.route)
                 },
                 onSignInClick = {
-                    // Переход на экран входа
-                    navController.navigate("login")
+                    navController.navigate(Screen.Login.route)
                 }
             )
         }
 
-        // Другие экраны (заглушки для примера)
-        composable("register_email") { /* Screen(...) */ }
-        composable("login") { /* Screen(...) */ }
+        // Экран регистрации по Email
+        composable(Screen.RegisterEmail.route) {
+            RegisterEmailScreen()
+        }
+
+        // Экран входа
+        composable(Screen.Login.route) {
+            LoginScreen()
+        }
+    }
+}
+
+// --- Временные заглушки для экранов (пока вы их не создали реально) ---
+
+@Composable
+fun RegisterEmailScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Screen: Register Email")
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Screen: Login")
     }
 }
