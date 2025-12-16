bd8ee82cd5e80ce5fa53cf31b3fca7cd67c7cf5a
Vater-v <v333iv@gmail.com>
2025-12-16 10:10:45 +0700

    Auto-update: 2025-12-16 10:10:45

--- mobile_app/app/src/main/java/com/hmuriy/shesh/ui/WelcomeScreen.kt
+++ mobile_app/app/src/main/java/com/hmuriy/shesh/ui/WelcomeScreen.kt
@@ -1,2 +1,31 @@
 package com.hmuriy.shesh.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.hmuriy.shesh.R
+
+@Composable
+fun WelcomeScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.god_mode_smirk),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        // Сюда можно добавить контент поверх фона
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreview() {
+    WelcomeScreen()
+}
