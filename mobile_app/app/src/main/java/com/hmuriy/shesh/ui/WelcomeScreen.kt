46d7d166543c963216003b08832691f51ac8fede
Vater-v <v333iv@gmail.com>
2025-12-16 12:23:06 +0700

    Auto-update: 2025-12-16 12:23:06

--- mobile_app/app/src/main/java/com/hmuriy/shesh/ui/WelcomeScreen.kt
+++ mobile_app/app/src/main/java/com/hmuriy/shesh/ui/WelcomeScreen.kt
@@ -2,6 +2,7 @@ package com.hmuriy.shesh.ui
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,39 +13,55 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hmuriy.shesh.ui.theme.*
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun WelcomeScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize() // Заполняем весь экран
-    ) {
-        // --- ВЕРХНЯЯ ЧЕТВЕРТЬ (Заголовок) ---
+    Column (modifier = Modifier.fillMaxSize(), Arrangement.SpaceAround){
         Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f), // Занимает 1 часть из 4 (25% экрана)
-            contentAlignment = Alignment.Center // Центрируем текст внутри этой четверти
-        ) {
-            Text(
-                text = "SHESH",
-                fontSize = 48.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        // --- ОСТАЛЬНЫЕ 3/4 (Подзаголовок) ---
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(3f), // Занимает 3 части из 4 (75% экрана)
-            contentAlignment = Alignment.TopCenter // Текст сверху этого блока
-        ) {
-            Text(
-                text = "Make it Shesh",
-                fontSize = 24.sp
-            )
-        }
+                .fillMaxWidth()        // Ширина на весь экран
+                .fillMaxHeight(0.2f)   // Высота 20% от доступного места (0.2f)
+                .background(Color.Gray) // Раскомментируйте, чтобы увидеть границы
+        ){Text("SHESH", fontSize = 48.sp, color = Theme.primary)}
+        Text("Java", fontSize = 28.sp)
+        Text("JavaScript", fontSize = 28.sp)
+        Text("Python", fontSize = 28.sp)
     }
+
+//    {
+//        Box(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .weight(1f),
+//
+//            contentAlignment = Alignment.TopCenter
+//        ) {
+//            Text(
+//                text = "SHESH",
+//                fontSize = 48.sp,
+//                fontWeight = FontWeight.Bold,
+//                // Устанавливаем фирменный "Электрический циан"
+//                color = CyberCyan,
+//
+//            )
+//        }
+//
+//        Box(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .weight(2f),
+//            contentAlignment = Alignment.BottomCenter
+//        ) {
+//            Text(
+//                text = "Make it Shesh",
+//                fontSize = 24.sp,
+//                // Устанавливаем основной светлый цвет текста
+//                color = TextWhite
+//            )
+//        }
+//    }
 }
 
 @Preview(showBackground = true)
